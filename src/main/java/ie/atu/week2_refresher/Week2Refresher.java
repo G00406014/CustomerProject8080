b93626cd77f06c3332b30994275d4f23cd325ba4
G00406014@atu.ie <g00406014@atu.ie>
2024-09-23 16:06:22 +0100

    Second commit

---
+++ src/main/java/ie/atu/week2_refresher/Week2Refresher.java
@@ -0,0 +1,41 @@
+package ie.atu.week2_refresher;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+@RestController
+public class Week2Refresher {
+
+    private List<Product> productist = new ArrayList<Product>();
+    @GetMapping("/Hello")
+    public String message()
+    {
+        return "Hello";
+
+    }
+    @GetMapping("/Hello/{name}")
+    public String myName(@PathVariable String name)
+    {
+        return "your name is " + name;
+    }
+    @GetMapping("/search")
+    public String search(@RequestParam String search1, @RequestParam () String search2)
+    {
+        return search1 + " " + search2;
+    }
+    @GetMapping
+    public List<Product> getProductList()
+    {
+        return productist;
+
+    }
+    @PostMapping("/products")
+    public Product addProduct(@RequestBody Product product)
+    {
+        productist.add(product);
+        return product;
+    }
+}
